c093adc892d756e714a5d8542519d60c2120cdd8
Prasetiaadi <pras.adi.prakasa@gmail.com>
2022-10-28 10:29:29 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,8 @@ const addTodo = () => {
     done: false,
     createdAt: new Date().getTime(),
   });
+  input_content.value = '';
+  input_category.value = null;
 };
 
 const removeTodo = (todo) => {
